fa67097c5b0b971d689cbdb6b7427e903d0ee7b1
alisalim <67149699+alisalim17@users.noreply.github.com>
2021-04-11 16:24:39 +0400

    Delete dist directory

--- dist/src/utils/pubsub.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pubsub = void 0;
-const graphql_subscriptions_1 = require("graphql-subscriptions");
-exports.pubsub = new graphql_subscriptions_1.PubSub();
-//# sourceMappingURL=pubsub.js.map
--- dist/utils/validations/pubsub.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphql_subscriptions_1 = require("graphql-subscriptions");
-exports.default = new graphql_subscriptions_1.PubSub();
-//# sourceMappingURL=pubsub.js.map
